9d75ea191443fecdf8ea10bcc2b00e329adbac6e
nesma440 <72654314+nesma440@users.noreply.github.com>
2020-11-14 12:42:45 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');   
 });
+
+Route::get('about',function(){
+    $name = 'Nesma Basheer';
+  $age = 20;
+
+   
+//   return view('about')->with('name', $name)->with('age',$age);
+return view('about', compact('name'));
+});
+ Route::post('send', function(Request $request){
+$name = $request->myname;
+     return view('about',compact('name'));
+});
+Route::get('tasks', function(){
+    $tasks = [
+       '1' => 'Task 1',
+       '2' => 'Task 2',
+       '3' => 'Task 3'
+    ];
+//dd($tasks);
+    return view('tasks',compact('tasks'));
+});
+Route::get('task/show/{id}', function($id){
+    $tasks = [
+        '1' => 'Task 1',
+        '2' => 'Task 2',
+        '3' => 'Task 3'
+     ];
+$task = $tasks[$id];
+//dd($task);
+    return view('show', compact('task'));
+
+});
+
